92c4bcdd36b962e321d5af58cfde96e99025ffac
Muhammad Asad Shahab <asadshahab02@gmail.com>
2022-09-29 00:04:35 +0500

    Commit

--- WebApplication/settings.py
+++ WebApplication/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-+tf^qy)d=c&g!)wsyqtwgq4rnnz0u%m30akn4)a@))2y4!f%4j"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','aisite3.herokuapp.com']
 
