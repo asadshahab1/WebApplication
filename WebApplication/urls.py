bcdf17c2d79052d368d9afac92188347cc480519
Muhammad Asad Shahab <asadshahab02@gmail.com>
2022-09-28 23:07:44 +0500

    Commit

--- WebApplication/urls.py
+++ WebApplication/urls.py
@@ -17,13 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.template.defaulttags import url
 from django.urls import path,include
-from django.views.static import serve
-from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("",include("AISite.urls")),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
 ]
